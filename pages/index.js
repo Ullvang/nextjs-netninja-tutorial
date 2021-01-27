e8eaa490b07fd9119a8ff6a94ea66edc2588d6a9
Jon Erik Ullvang <jon.erik.ullvang@gmail.com>
2021-01-27 21:14:23 +0100

    #4

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import styles from "../styles/Home.module.css";
@@ -8,6 +9,7 @@ export default function Home() {
     <div>
       <Navbar />
       <h1>Homepage2</h1>
+      <Link href="/ninjas">See Ninja Listing</Link>
       <Footer />
     </div>
   );
