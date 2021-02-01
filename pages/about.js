d9e43e757792416ab07e7fe0cac37881698f6c73
Jon Erik Ullvang <jon.erik.ullvang@gmail.com>
2021-02-01 18:14:09 +0100

    #9

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,17 @@
+import Head from "next/head";
+
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p>Cool!!!</p>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>Cool!!!</p>
+      </div>
+    </>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -6,17 +6,23 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Homepage2</h1>
-      <p className={styles.text}>
-        Lorem ipsum database crash fork css DOM. Lorem ipsum database crash fork
-        css DOM. Lorem ipsum database crash fork css DOM. wow nice Lorem ipsum
-        database crash fork css DOM. Lorem ipsum database crash fork css DOM.
-        Lorem ipsum database crash fork css DOM
-      </p>
-      <Link href="/ninjas">
-        <a className={styles.btn}>See Ninja Listing</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1 className={styles.title}>Homepage2</h1>
+        <p className={styles.text}>
+          Lorem ipsum database crash fork css DOM. Lorem ipsum database crash
+          fork css DOM. Lorem ipsum database crash fork css DOM. wow nice Lorem
+          ipsum database crash fork css DOM. Lorem ipsum database crash fork css
+          DOM. Lorem ipsum database crash fork css DOM
+        </p>
+        <Link href="/ninjas">
+          <a className={styles.btn}>See Ninja Listing</a>
+        </Link>
+      </div>
+    </>
   );
 }
