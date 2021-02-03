f7dd5033e63fa683509e3847ae99e6415f6dde02
Jon Erik Ullvang <jon.erik.ullvang@gmail.com>
2021-02-03 16:54:02 +0100

    #11

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import styles from "../../styles/Ninjas.module.css";
 export const getStaticProps = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
@@ -15,11 +16,11 @@ const Index = ({ ninjas }) => {
     <div>
       <h1>Index ninjas</h1>
       {ninjas.map((ninja) => (
-        <div key={ninja.id}>
+        <Link href={"/ninjas/" + ninja.id} key={ninja.id}>
           <a className={styles.single}>
             <h3>{ninja.name}</h3>
           </a>
-        </div>
+        </Link>
       ))}
     </div>
   );
